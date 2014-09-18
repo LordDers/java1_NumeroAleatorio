5b6c24eafb49a8a130d2730ae45f519dc8c2e51f
Ruben <lordders@gmail.com>
2014-09-18 09:16:55 +0200

    subida archivo

---
+++ NumeroAleatorio.java
@@ -0,0 +1,10 @@
+public class NumeroAleatorio {
+static int numero;
+
+public static void main(String args[]){
+
+ numero = (int) (Math.random() *20) + 1; 
+ System.out.print(numero);
+ System.out.println(""); 
+ }
+}
