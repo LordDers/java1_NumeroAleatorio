cc412fe8a43e4588993ae8b876c3508f91b3cac3
Ruben <lordders@gmail.com>
2014-09-18 09:21:59 +0200

    subida archivo java

--- NumeroAleatorio.java
+++ NumeroAleatorio.java
@@ -1,10 +1,11 @@
-public class NumeroAleatorio {
-static int numero;
+public class NumeroAleatorio 
+{
+	static int numero;
+	public static void main(String args[]) // Obligatorio para poder ejecutar codigo Java
+	{
 
-public static void main(String args[]){
-
- numero = (int) (Math.random() *20) + 1; 
- System.out.print(numero);
- System.out.println(""); 
- }
+		numero = (int) (Math.random() *20) + 1; // Numero aleatorio hasta el 20
+		System.out.print(numero);
+		System.out.println(""); // Imprime en blanco y salta de linea
+	}
 }
